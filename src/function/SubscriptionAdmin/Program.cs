7cdd5ea998f749979e5fd40d2c3cde7b91130f45
Hugo Girard <hugirard@microsoft.com>
2023-06-06 16:28:41 -0400

    added app insights

--- src/function/SubscriptionAdmin/Program.cs
+++ src/function/SubscriptionAdmin/Program.cs
@@ -19,6 +19,7 @@
 */
 using Azure.Messaging.ServiceBus;
 using Azure.Messaging.ServiceBus.Administration;
+using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
